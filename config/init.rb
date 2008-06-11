9f4cc48e19ba5889709cc22b134921ef33acc062
Foy Savas <foysavas@gmail.com>
2008-06-11 09:50:30 -0400

    Import old views

--- config/init.rb
+++ config/init.rb
@@ -58,6 +58,9 @@ Gem.path.unshift(Merb.root / "gems")
 # OR
 # dependencies "RedCloth" => "> 3.0", "ruby-aes-cext" => "= 1.0"
 dependency 'merb-action-args'
+dependency 'merb-assets'
+dependency 'merb-haml'
+dependency 'merb_helpers'
 dependency 'dm-timestamps'
 dependency 'dm-validations'
 dependency 'dm-aggregates'
