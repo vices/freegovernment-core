ec6c566558756cfd575c987070b9d199624451a0
Foy Savas <foysavas@gmail.com>
2008-06-13 22:29:30 -0400

    Add Jayme's Poll code and spec

---
+++ app/controllers/polls.rb
@@ -0,0 +1,69 @@
+class Polls < Application
+  
+  def index
+    case params['sort_by']
+      when 'question'
+        @sort_by = :question
+      else
+        @sort_by = :created_at
+    end
+    case params['direction']
+      when 'desc'
+        @direction = 'desc'
+        @order = @sort_by.desc
+      else
+        @direction = 'asc'
+        @order = @sort_by.asc
+    end
+  
+    @polls_page = Poll.all(:order => [@order])
+    render
+  end
+  
+  def show
+    @poll = Poll.first(:id => params[:id])
+    display @poll
+    render
+  end
+  
+  def new
+    @new_poll = Poll.new
+    @new_forum = Forum.new
+    @new_topic = Topic.new
+    @new_post = Post.new
+    render
+  end
+  
+  def create(poll)
+    @new_poll = Poll.new(poll)
+    @new_forum = Forum.new
+    @new_topic = Topic.new
+    @new_post = Post.new
+    if verify_recaptcha(params[:recaptcha])
+      if @new_poll.valid?(:before_poll_creation) 
+        @new_poll.save
+        @new_forum.save
+        @new_topic.save
+        @new_post.save
+        redirect url(:polls)
+      else
+        render :new
+      end
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  
+  end
+  
+  def update
+  
+  end
+  
+  def destroy
+  
+  end
+  
+end
