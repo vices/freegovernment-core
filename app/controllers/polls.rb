ec61c9b2a01403c996f841e44e335fc8d7461e0a
Jayme Fosa <jaymecfosa@yahoo.com>
2008-06-18 13:12:28 -0400

    polls

--- app/controllers/polls.rb
+++ app/controllers/polls.rb
@@ -1,6 +1,7 @@
 class Polls < Application
   before :login_required, :only => %w{ new create }
   before :find_poll, :only => %w{ show }
+  before Proc.new{ @nav_active = :polls }
   
   def index
     case params['sort_by']
@@ -27,19 +28,22 @@ class Polls < Application
   end
   
   def create
+  #problem with merge here
     @new_poll = Poll.new(params[:poll].merge(:user_id => session[:user_id]))
 
     if verify_recaptcha(params[:recaptcha])
-      if @new_poll.save
-        @new_forum = Forum.new(:name => @new_poll.question, :poll_id => @new_poll.id)
-        @new_forum.save
-        @new_topic = Topic.new(:name => "Comments", :forum_id => @new_forum.id,
-        :user_id => @new_poll.user_id)
-        @new_topic.save
-        redirect url(:polls)
-      else
-        render :new
-      end
+      if @new_poll.valid?(:before_poll_creation)
+        if @new_poll.save
+          @new_forum = Forum.new(:name => @new_poll.question, :poll_id => @new_poll.id)
+          @new_forum.save
+          @new_topic = Topic.new(:name => "Comments", :forum_id => @new_forum.id,
+          :user_id => @new_poll.user_id)
+          @new_topic.save
+          redirect url(:polls)
+        else
+          render :new
+        end
+      end  
     else
       render :new
     end
