db7495328034050d058a8cfa71815aea8b494976
Tomasz Werbicki <neaf@19feelings.com>
2008-07-02 00:09:00 +0200

    Ajaxify fallback form.

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -2,16 +2,29 @@ var Feedback = {
 	init: function() {
 		$('#feedback_link').css('display', 'block').click(this.clickIt);
 		$('#feedback_form .close').click(function() {
-			$('#feedback_form').css('display', 'none');
+			Feedback.closeForm();
 		});
-
+		$('#feedback_form').ajaxForm(function() {
+			$('#feedback_form .submit').text('Thanks!');
+			setTimeout(Feedback.finishSending, 1000);
+		});
 	},
 	clickIt: function() {
 		if ($('#feedback_form').css('display') == 'none') {
 			$('#feedback_form').css('display', 'block');
 		} else {
-			$('#feedback_form').css('display', 'none');
+			Feedback.closeForm();
 		}
+	},
+	closeForm: function() {
+		$('#feedback_form').css('display', 'none');
+	},
+	finishSending: function() {
+		Feedback.closeForm();
+		setTimeout(function() {
+			$('#feedback_form .submit').text('Send Feedback!');
+			$('#feedback_form textarea').val('');
+		}, 500);
 	}
 }
 
