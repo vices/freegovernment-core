b74a1bfb3fc755ddf014ebdbcd4861fb4722cb44
Tomasz Werbicki <neaf@19feelings.com>
2008-07-02 19:30:31 +0200

    Add new requests button to account bar.

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -10,11 +10,6 @@ class Home < Application
   end
 
   def start
-    if !@current_user.person_id.nil?
-      @new_cr_count = ContactRelationship.count(:contact_id => @current_user.person_id, :is_accepted => 0)
-    else
-      @new_gr_count = GroupRelationship.count(:group_id => @current_user.group_id, :is_accepted => 0)
-    end
     render
   end
 
