ebcf04493106ff8b9760befc2a12360f31b9f6c8
Tomasz Werbicki <neaf@19feelings.com>
2008-07-02 11:27:33 +0200

    Styling fixes.

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,9 +1,11 @@
 class Feedback
   include DataMapper::Resource
-
+  include DataMapper::Timestamp
+  
   property :id, Integer, :serial => true
   property :message, String
   property :user_id, Integer
   property :controller, String
   property :action, String
+  property :created_at, DateTime
 end
