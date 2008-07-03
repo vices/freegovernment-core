8f85e055a8f562045a5ce43833ddb89d3ec047f3
Foy Savas <foysavas@gmail.com>
2008-07-03 12:12:18 -0400

    Add BillSection model

---
+++ app/models/bill_section.rb
@@ -0,0 +1,12 @@
+class BillSection
+  include DataMapper::Resource
+  include DataMapper::Timestamps
+
+  property :id, Integer, :serial => true
+  property :bill_id, Integer, :nullable => false
+  property :forum_id, Integer, :nullable => false
+  property :topic_id, Integer, :nullable => false
+  property :title, DM::Text, :nullable => false
+  property :text, DM::Text
+
+end
