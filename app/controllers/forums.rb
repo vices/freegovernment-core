1820ac0ee795bb19a65f33367b0318f9e7f5de71
Tomasz Werbicki <neaf@19feelings.com>
2008-07-03 14:25:17 +0200

    Use forked merb_paginate to fix forums pagination.

--- app/controllers/forums.rb
+++ app/controllers/forums.rb
@@ -16,25 +16,28 @@ class Forums < Application
   
   def group_index
     @what = 'Group'
-    @forums_page = Forum.paginate(:page => params[:group_page], :per_page => 20, :conditions => {:group_id.not => nil})
+    @route = :group_index
+    @forums_page = Forum.paginate(:page => params[:page], :per_page => 20, :conditions => {:group_id.not => nil})
     render :template => 'forums/index_specified.html'
   end
 
   def poll_index
     @what = 'Poll-Related'
-    @forums_page = Forum.paginate(:page => params[:group_page], :per_page => 20, :conditions => {:poll_id.not => nil, :bill_id => nil})
+    @route = :poll_index
+    @forums_page = Forum.paginate(:page => params[:page], :per_page => 5, :conditions => {:poll_id.not => nil, :bill_id => nil})
     render :template => 'forums/index_specified.html'
   end
 
   def bill_index
     @what = 'Bill-Related'
-    @forums_page = Forum.paginate(:page => params[:group_page], :per_page => 20, :conditions => {:bill_id.not => nil})
+    @route = :bill_index
+    @forums_page = Forum.paginate(:page => params[:page], :per_page => 20, :conditions => {:bill_id.not => nil})
     render :template => 'forums/index_specified.html'
   end
 
   # Shows forum / Lists paginated topics of forum
   def show
-    @topics_page = Topic.paginate(:page => params[:page], :per_page => 20, :conditions => {:forum_id => @forum.id})
+    @topics_page = Topic.paginate(:page => params[:page], :per_page => 5, :conditions => {:forum_id => @forum.id})
     render
   end
   
