3c3b608fd9de5715a849d1050f04a5bbdd0ea986
forsakensoul <forsakensoul.im@gmail.com>
2008-07-04 18:31:55 +0300

    bills order

--- app/controllers/bills.rb
+++ app/controllers/bills.rb
@@ -4,8 +4,14 @@ class Bills < Application
   before :find_bill, :only => %w{ show }
 
   def index
-    @title = 'Latest bills'
-    @bills_page = Bill.paginate(:per_page => 8, :page => params[:page], :order => [:created_at.desc])
+    @pagination_params = {}
+    if search_conditions[:conditions].nil?
+      @title = 'Latest bills'
+    else
+      @title = 'Pills for query: <span>%s</span>' % params[:search][:question]
+      @pagination_params = { 'search[question]' => params[:search][:question] }
+    end
+    @bills_page = Bill.paginate({:page => params[:page], :per_page => 8}.merge(search_conditions).merge(order_conditions))
     render
   end
 
@@ -45,6 +51,36 @@ class Bills < Application
   end
 
   private
+
+  def order_conditions
+    params['sort_by'] = '' if params['sort_by'].nil?
+    sort_by = case params['sort_by'].downcase
+    when 'title'
+      :title
+    when 'created_at'
+      :created_at
+    else
+      :id
+    end
+
+    params['direction'] = '' if params['direction'].nil?    
+    order = params['direction'].downcase == 'desc' ? sort_by.desc : sort_by.asc
+    
+    {:order => [order]}
+  end
+
+  def search_conditions
+    out = {}
+    unless params[:search].nil? || params[:search].empty?
+      params[:search].each_pair do |col, val|
+        out[col.to_sym.like] = "%#{val}%"
+      end
+    end
+    if !out.empty?
+      out = {:conditions => out}
+    end
+    out
+  end
 
   def find_bill
     if(@bill = Bill.first(:id => params[:id])).nil?
